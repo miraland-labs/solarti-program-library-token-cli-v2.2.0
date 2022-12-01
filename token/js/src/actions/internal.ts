1de3df660d86a8c31842e696674693ca408fac0e
Miraland Labs <miraland.labs@gmail.com>
2022-12-01 20:55:06 +0800

    initial upload after download and rebase

---
+++ token/js/src/actions/internal.ts
@@ -0,0 +1,9 @@
+import type { Signer } from '@solarti/web3.js';
+import { PublicKey } from '@solarti/web3.js';
+
+/** @internal */
+export function getSigners(signerOrMultisig: Signer | PublicKey, multiSigners: Signer[]): [PublicKey, Signer[]] {
+    return signerOrMultisig instanceof PublicKey
+        ? [signerOrMultisig, multiSigners]
+        : [signerOrMultisig.publicKey, [signerOrMultisig]];
+}
